9c3ce4dc024a77a44a4d1e1746eaf836abe22b2a
Đoàn Kiều Mỹ Ngọc <97938927+DoanKieuMyNgoc@users.noreply.github.com>
2022-10-16 13:14:36 +0700

    sua_3

--- TH6/input_list/App.js
+++ TH6/input_list/App.js
@@ -1,62 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Input_List from './Input_List';
-import { useState } from 'react';
-export default function App() {
-  const Stack = createNativeStackNavigator()
+import React, { useState } from "react";
+import {
+  SafeAreaView, StyleSheet, View, Text,
+  FlatList, Modal, TouchableOpacity, TextInput
+} from "react-native"
+
+const DATA = [
+  { id: 1, text: "One" },
+  { id: 2, text: "Two" },
+  { id: 3, text: "Three" },
+  { id: 4, text: "Four" },
+  { id: 5, text: "Five" }
+
+]
+const App = () => {
+  const [data, setData] = useState(DATA)
+
+  const [isModalisible, setisModalisible] = useState(false)
+
+  const [inputText, setinputText] = useState()
+
+  const [edititem, seteditItem] = useState()
+
+  const [isRender, setisRender] = useState(false)
+
+  const onPressItem = (item) => {
+    setisModalisible(true)
+    setinputText(item.text)
+    seteditItem(item.id)
+  }
+  const onPressSave = () => {
+    const newData = DATA.map(item => {
+      if (item.id == edititem) {
+        item.text = inputText;
+        return item
+      }
+      return item
+    })
+    setData(newData)
+    setisModalisible(false)
+    setisRender(!isRender)
+  }
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity
+        style={styles.item}
+        onPress={() => onPressItem(item)}>
+        <Text style={styles.text}> {item.text}</Text>
+      </TouchableOpacity>
+    )
+  }
   return (
-    <View style={styles.container}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Input">
-            <Stack.Screen name='InputScreeen' component={InputScreeen} />
-            <Stack.Screen name='ListScreen' component={ListScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        extraData={isRender}
+      />
+      <Modal
+        animationType="fade"
+        visible={isModalisible}
+        onRequestClose={() => setisModalisible(false)}
+      >
+        <View style={styles.modalview}>
+          <Text style={styles.text}>Change Text: </Text>
+          <TextInput style={styles.textInput}
+            onChangeText={(text) => setinputText}
+            defaultValue={inputText}
+            editable={true}
+            multilin={false}
+            maxLength={200} />
+          <TouchableOpacity style={styles.touchableSave}
+            onPress={() => onPressSave()}>
+            <Text style={styles.text}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
-});
-const InputScreeen = ({ navigator }) => {
-  const [Ten, setTen] = useState('')
-  const [NamSinh, setNamSinh] = useState('')
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Nhập dữ liệu</Text>
-      <TextInput style={{ border: 1 }}
-        onChangeText={(txt) => setTen(txt)}
-        value={Ten}
-        placeholder="Nhập tên" />
-      <TextInput style={{ border: 1 }}
-        onChangeText={(txt) => setNamSinh(txt)}
-        value={NamSinh}
-        placeholder="Nhập năm sinh" />
-      <Button
-        onPress={() => navigation.navigate('ListScreen', {
-          Ten, NamSinh
-        })}
-        title="Gọi đến từ Activity khác"
-      />
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
+    alignItems: "flex-start"
+  },
+  text: {
+    marginVertical: 30,
+    fontSize: 25,
+    fontWeight: "bold",
+    marginLeft: 10
+  },
+  modalview: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  touchableSave: {
+    backgroundColor: "orange",
+    paddingHorizontal: 100,
+    alignItems: "center",
+    marginTop: 20
+  },
+  textInput: {
+    width: "90%",
+    height: 70,
+    borderColor: "grey",
+    borderWidth: 1,
+    fontSize: 25
+  }
+})
 
-    </View>
-  )
-}
-const ListScreen = ({ route, navigation }) => {
-  const { Ten } = route.params;
-  const { NamSinh } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{Ten}</Text>
-      <Text>{NamSinh}</Text>
-    </View>
-  )
-}
+export default App;
